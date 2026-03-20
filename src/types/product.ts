dd4489144d5ae058a7240e0f745ea5b75ff53e2f
alfrusmi-tech <alfrusmi@gmail.com>
2026-03-20 11:10:46 +0530

    message

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,10 +1,13 @@
 export interface Product {
-
   id: number
   title: string
   description: string
   price: number
-  thumbnail: string
+  discountPercentage: number
+  rating: number
+  stock: number
+  brand: string
   category: string
-
+  thumbnail: string
+  images: string[]
 }
